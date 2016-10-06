04001a16cdf908696ae3552ac6a4f60131fd522d
ruzhaa <ruzha_bobotanova@abv.bg>
2016-10-06 15:10:45 +0300

    Tic-Tac-Toe is not ready

--- week0/1-Tic-Tac-Toe/game.js
+++ week0/1-Tic-Tac-Toe/game.js
@@ -1,7 +1,7 @@
 "use strict";
 var prompt = require("sync-prompt").prompt;
 
-function printBoard(board) {
+function printBoard (board) {
   var
     i = 0,
     n = board.length;
@@ -12,6 +12,40 @@ function printBoard(board) {
   }
 }
 
+function printPlayer (player) {
+  console.log("This is " + player.name + " -> " + player.sign);
+}
+
+function checkPosition (position) {
+  if ( position > 3 || position < 1) {
+    console.log("This position is outside.");
+    return true;
+  } else if (isNaN(position)) {
+    console.log("Entrer again number for position")
+  }
+  return false;
+}
+
+function checkPositionOfBoard (x, y, board, player) {
+  console.log(player);
+  if (board[x - 1][y - 1] != 'X' && board[x - 1][y - 1] != 'O') {
+    board[x - 1][y - 1] = player.sign;
+  } else {
+    console.log("This place is occupied");
+    return true;
+  }
+}
+
+function checkWinPositionHorisontal (board, player1) {
+  if (player1) {
+    // Player 1 -> (X)
+
+  } else {
+    // Player 2 -> (O)
+  }
+}
+
+
 // entry point for the game
 function gameLoop() {
   var
@@ -19,22 +53,49 @@ function gameLoop() {
               ["*", "*", "*"],
               ["*", "*", "*"] ],
     xTurn = true,
-    position = null;
+    player = true;
+
+    var nicknameFirstPlayer = { name: prompt('Enter your nickname: '), sign: 'X' },
+        nicknameSecondPlayer = { name: prompt('Enter your nickname: '), sign: 'O' };
+
   while(true) {
 
-    console.log("This is the current state of the board:");
-    printBoard(board);
-
     if(xTurn) {
-      console.log("Place for x");
+
+      if (player) {
+        printPlayer(nicknameFirstPlayer);
+      } else {
+        printPlayer(nicknameSecondPlayer);
+      }
+
+      console.log("This is the current state of the board:");
+      printBoard(board);
+
+      var x = prompt("Enter x> ");
+      while (checkPosition(x)) {
+        x = prompt("Enter x> ");
+      }
     } else {
-      console.log("Place for y");
+      var y = prompt("Enter y> ");
+      while (checkPosition(y)) {
+        y = prompt("Enter y> ");
+      }
     }
 
-    // this is blocking prompt
-    position = prompt("x y>");
-    console.log(position);
+    // ако не се въвежда х => 
+    // се е въвело у и ще правя проверка за въведената позиция и после ще сменя xTurn
+    if (!xTurn) {
+      // change player
+      player = !player;
+    } else {
+      if (player) {
+        checkPositionOfBoard(x, y, board, nicknameFirstPlayer);
+      } else {
+        checkPositionOfBoard(x, y, board, nicknameSecondPlayer);
+      }
+    }
     xTurn = !xTurn;
+
   }
 }
 
